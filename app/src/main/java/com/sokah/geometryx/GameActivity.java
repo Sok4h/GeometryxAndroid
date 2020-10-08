af045116a78163bf414b0ae0c7ec7915cf40cde8
Sok4h <55671660+Sok4h@users.noreply.github.com>
2020-10-08 16:45:07 -0500

    Disparo + super disparo

--- app/src/main/java/com/sokah/geometryx/GameActivity.java
+++ app/src/main/java/com/sokah/geometryx/GameActivity.java
@@ -3,17 +3,61 @@ package com.sokah.geometryx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class GameActivity extends AppCompatActivity {
+import com.google.gson.Gson;
 
+public class GameActivity extends AppCompatActivity  implements OnMessageListener{
+
+    Button shoot,superShoot;
+    Gson gson = new Gson();
+    TCP_Singleton tcp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Terminar de configurar modo inmersivo
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+
         setContentView(R.layout.activity_game);
+        tcp=TCP_Singleton.getInstance();
+        tcp.SetObserver(this);
+        shoot=findViewById(R.id.buttonShoot);
+        superShoot=findViewById(R.id.buttonSpecial);
+
+        shoot.setOnClickListener(
+
+                (v)->{
+
+                    Shoot tempShoot = new Shoot();
+                    String message = gson.toJson(tempShoot);
+                    tcp.SendMessage(message);
+                    //Log.e("TAG", "Disparé");
+
+                }
+        );
+
+        superShoot.setOnClickListener(
+
+                (v)->{
+
+                    Shoot tempShoot = new Shoot();
+                    tempShoot.setSuperShoot(true);
+                    String message = gson.toJson(tempShoot);
+                    tcp.SendMessage(message);
+                    //Log.e("TAG", "Disparé super" );
+
+                }
+        );
+    }
+
+
+    @Override
+    public void OnMessage(String msg) {
+
     }
 }
---
+++ app/src/main/java/com/sokah/geometryx/Shoot.java
@@ -0,0 +1,19 @@
+package com.sokah.geometryx;
+
+public class Shoot {
+	
+	private String type = "Shoot";
+	private boolean superShoot;
+	
+	public Shoot() {
+		
+	}
+
+	public boolean isSuperShoot() {
+		return superShoot;
+	}
+
+	public void setSuperShoot(boolean superShoot) {
+		this.superShoot = superShoot;
+	}
+}
