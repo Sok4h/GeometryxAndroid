43f6ee8048821db80cad366d694cd3bc208a8aa3
AlejandroVal99 <joalevalverde@hotmail.com>
2020-10-13 20:56:23 -0500

    move in process

--- app/src/main/java/com/sokah/geometryx/view/GameActivity.java
+++ app/src/main/java/com/sokah/geometryx/view/GameActivity.java
@@ -6,7 +6,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
@@ -21,7 +20,6 @@ import com.sokah.geometryx.comunnication.TCP_Singleton;
 import com.sokah.geometryx.events.OnMessageListener;
 import com.sokah.geometryx.model.Direction;
 import com.sokah.geometryx.model.Shoot;
-import com.sokah.geometryx.model.Vibration;
 
 public class GameActivity extends AppCompatActivity implements OnMessageListener, SensorEventListener {
 
@@ -29,25 +27,40 @@ public class GameActivity extends AppCompatActivity implements OnMessageListener
     private Gson gson = new Gson();
     private TCP_Singleton tcp;
     private SensorManager sensorManager;
-    private Sensor acelerometer;
+    private Sensor sensor;
     private Vibrator vibrator;
+    private float[] mRotationMatrix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Terminar de configurar modo inmersivo
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        /*
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        tcp = TCP_Singleton.getInstance();
-        tcp.SetObserver(this);
+
         acelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, acelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         if (acelerometer == null) {
 
             Log.e("TAG", "paila el sensor");
-        }
+        }*/
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        tcp = TCP_Singleton.getInstance();
+        tcp.SetObserver(this);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mRotationMatrix = new float[16];
+        mRotationMatrix[ 0] = 1;
+        mRotationMatrix[ 4] = 1;
+        mRotationMatrix[ 8] = 1;
+        mRotationMatrix[12] = 1;
+
+
+
         setContentView(R.layout.activity_game);
         tcp = TCP_Singleton.getInstance();
         tcp.SetObserver(this);
@@ -81,36 +94,39 @@ public class GameActivity extends AppCompatActivity implements OnMessageListener
     }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, acelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
+
 
 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        //.e("X", String.valueOf(event.values[0]));
-        //Log.e("Y", String.valueOf(event.values[1]));
-        //Log.e("Z", String.valueOf(event.values[2]));
-        float x= event.values[1];
+        float axisX = event.values[0];
+        float axisY = event.values[1];
+        float axisZ = event.values[2];
+        double angleXY = Math.toDegrees(Math.atan(axisY/axisX));
+        int anguloX = (int)angleXY;
 
+        float[] results = new float[3];
+        SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
+        SensorManager.getOrientation(mRotationMatrix, results);
+        double angleZ = Math.toDegrees(results[2]);
+       int anguloZ = (int)angleZ;
 
         new Thread(
                 () -> {
                     boolean moveX = true;
 
                     while (moveX) {
-                        if (x > 4) {
+
+                        if (anguloX > 50) {
 
 
                             Log.e("TAG", "Me muevo derecha");
                             Direction dir = new Direction(-1);
                             String msgDir = gson.toJson(dir);
                             tcp.SendMessage(msgDir);
-                        } else if (x < -4) {
+                        } else if (anguloX < 40) {
 
                             Log.e("TAG", "Me muevo izquierda");
                             Direction dir = new Direction(1);
@@ -120,7 +136,7 @@ public class GameActivity extends AppCompatActivity implements OnMessageListener
                             moveX = false;
                         }
                         try {
-                            Thread.sleep(2000);
+                            Thread.sleep(450);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -129,19 +145,19 @@ public class GameActivity extends AppCompatActivity implements OnMessageListener
 
         ).start();
 
-       /*/ new Thread(
+        new Thread(
                 () -> {
                     boolean movey = true;
 
                     while (movey) {
 
-                        if (event.values[2] > -3) {
+                        if ( anguloZ> -65) {
 
                             Log.e("TAG", "Me muevo Arriba");
                             Direction dir = new Direction(2);
                             String msgDir = gson.toJson(dir);
                             tcp.SendMessage(msgDir);
-                        } else if (event.values[2] < 3) {
+                        } else if (anguloZ < -85) {
 
                             Log.e("TAG", "Me muevo Abajo");
                             Direction dir = new Direction(-2);
@@ -152,7 +168,7 @@ public class GameActivity extends AppCompatActivity implements OnMessageListener
                         }
 
                         try {
-                            Thread.sleep(2000);
+                            Thread.sleep(450);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -160,7 +176,7 @@ public class GameActivity extends AppCompatActivity implements OnMessageListener
                 }
 
         ).start();
-        */
+
     }
 
     @Override
